72203c6abe067d0d5fc5ecdfc0e4aa8a79863511
Sid <k.siddhardhareddy112@gmail.com>
2026-02-27 02:20:34 +0530

    implement responsive contact form with Web3Forms

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,21 @@ import TrustedBy from "./components/TrustedBy";
 import Services from "./components/Services";
 import OurWork from "./components/OurWork";
 import Teams from "./components/Teams";
+import ContactUs from "./components/ContactUs";
+import {Toaster} from "react-hot-toast"
 
 const App = () => {
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
   return (
     <div className="dark:bg-black relative ">
+      <Toaster />
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero />
       <TrustedBy />
       <Services />
       <OurWork />
       <Teams />
+      <ContactUs />
       
     </div>
   );
