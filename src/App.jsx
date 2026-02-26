226bd2b7ef42b509cd8257f4ebc952942e775049
Sid <k.siddhardhareddy112@gmail.com>
2026-02-26 20:14:50 +0530

    Awesome Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
 
 const App = () => {
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
   return (
     <div className="dark:bg-black relative ">
       <Navbar theme={theme} setTheme={setTheme} />
+      <Hero />
     </div>
   );
 };
