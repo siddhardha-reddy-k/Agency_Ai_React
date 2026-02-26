d64e4b0ad1acf3f4747713a5b194b6ee8b2d0a5e
Sid <k.siddhardhareddy112@gmail.com>
2026-02-26 20:27:07 +0530

    Add trusted by section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-
+import TrustedBy from "./components/TrustedBy";
 
 const App = () => {
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
@@ -9,6 +9,7 @@ const App = () => {
     <div className="dark:bg-black relative ">
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero />
+      <TrustedBy />
     </div>
   );
 };
